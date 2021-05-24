033f8cfb684eced4543726a7156409df1f221a88
Duvi Kappel <duvikappel@gmail.com>
2021-05-23 21:51:53 -0400

    HW46

--- python/46/Selection_Sort.py
+++ python/46/Selection_Sort.py
@@ -1,6 +1,7 @@
 import random
 
 my_list = []
+list2=[]
 
 def fill_list(list):
     length=10
@@ -20,7 +21,24 @@ def selection_sort(list):
         list[min_index] = list[i]
         list[i] = temp
     print(list)             
-          
+      
 
-selection_sort(my_list)  
- 
+selection_sort(my_list) 
+
+def selection_sort2(list):
+    divide_index=0
+    while divide_index < len(list)-1:
+        min_index = divide_index
+        for i in range(divide_index + 1, len(list)):
+          if list[i] < list[min_index]:
+              min_index=i
+        if list[min_index] < list[divide_index]:   
+            temp=list[divide_index]
+            list[divide_index]=list[min_index]
+            list[min_index] = temp      
+        divide_index =+1
+    print(list) 
+    
+
+fill_list(list2)
+selection_sort2(list2)  
