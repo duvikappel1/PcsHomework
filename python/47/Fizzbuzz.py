19e14a3c58053d3eb2450a66067eac3bc3a05527
Duvi Kappel <duvikappel@gmail.com>
2021-05-25 19:55:18 -0400

    HW47

---
+++ python/47/Fizzbuzz.py
@@ -0,0 +1,13 @@
+for i in range(1,100):
+    if i % 3 == 0 and i % 5 == 0:
+      x = 'FizzBuzz'
+    elif i % 3 == 0 :
+        x = 'Fizz'
+    elif i % 5 == 0 :
+        x = 'Buzz' 
+    else:
+        x = i       
+    print(x) 
+         
+
+      
