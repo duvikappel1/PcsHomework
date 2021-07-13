389405812e040c273b8eae23c9a5a25097cd936a
Duvi Kappel <duvikappel@gmail.com>
2021-07-13 11:44:01 -0400

    HW60

---
+++ js/60/one.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var name="Duvi Kappel";
+var email="duvikappel@gmail.com";
+
+var yourAge = prompt("How old are you?");
+console.log("You are" + yourAge + "years old.");
---
+++ js/60/two.js
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log("My name is" + name);
+console.log("My email is" + email);
+
