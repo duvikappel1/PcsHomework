1934e1c5e07b1a93e4b270f953049e7f07942985
Duvi Kappel <duvikappel@gmail.com>
2021-08-18 17:27:40 -0400

    HW62 Complete

--- js/60/two.js
+++ js/60/two.js
@@ -3,3 +3,13 @@
 console.log("My name is" + name);
 console.log("My email is" + email);
 
+function getBestGreeter(name) {
+    return function () {
+      console.log(`Hello ${name}`);
+    };
+  }
+  let x=getBestGreeter('Joe');
+  let bestGreeter = getBestGreeter('Joe');
+  bestGreeter();
+  console.log(x);
+  x();
--- js/62/hw.js
+++ js/62/hw.js
@@ -1,7 +1,7 @@
 
 
 const private=(function(){
-    'use strict'; 
+    'use strict';
 const days=['Sunday','Monday','Tuesday'];
 
 function getDay(index){
@@ -23,3 +23,42 @@ return {
 }());
 console.log(private.getDay(1));
 console.log(private.getIndex('Sunday'));
+
+
+
+function r(){
+    'use strict';
+    
+    let years;
+    let rate;
+
+    function setYears(y){
+        years=y;
+    }
+    function setRate(r){
+        rate=r;
+    }
+    function getInterest(p){
+        let principle;
+        principle=p;
+        for(let i=0; i<years; i++ ){
+            principle += principle * rate;
+        }
+         return principle-p;
+    }
+    years
+
+    return{
+        setRate: setRate,
+        setYears: setYears,
+        getInterest: getInterest
+    }
+};
+const c=r();
+c.setYears(2);
+c.setRate(.1);
+console.log(c.getInterest(100));
+const q=r();
+q.setYears(3);
+q.setRate(.2)
+console.log(q.getInterest(200));
