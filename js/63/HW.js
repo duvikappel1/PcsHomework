dcd1db9ca6750806f43de5001e73afb5d9785cc3
Duvi Kappel <duvikappel@gmail.com>
2021-08-23 00:08:40 -0400

    HW63

--- js/63/HW.js
+++ js/63/HW.js
@@ -10,6 +10,9 @@
             }
         }
     }
+    function transaction2(amount){
+        this.balance+=amount;
+    }
     
         const account1=createAccount(500);
         const account2=createAccount(1000);
@@ -17,8 +20,10 @@
         account1.transaction(10);
         account2.transaction(10);
         console.log(account1, account2);
-        console.log(account1, account2);
         
+        transaction2.call(account1, 10);
+        transaction2.call(account2, 10);
+        console.log(account1, account2);
     })();
 
     
