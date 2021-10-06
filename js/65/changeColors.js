8763d0036c8ddcf3871d403c4532c668646faf76
Duvi Kappel <duvikappel@gmail.com>
2021-10-06 13:28:34 -0400

    HW65 Completed

--- js/65/changeColors.js
+++ js/65/changeColors.js
@@ -9,15 +9,18 @@
 }
 
      const colors=['red','blue','yellow'];
-     const backgroundColor=['blue','green','red']
-
-     setInterval(() => {
-        for(let i=0; i < 3; i++){
-        theButton.addEventListener('click', ()=>{
-            setCss(document.body, 'color', colors[i]);
-            setCss(document.body, 'backgroundColor', backgroundColor[i]);
-            });
-        }  
-     }, 5000);
      
+     let i=0;
+     theButton.addEventListener('click', ()=>{
+     setInterval(() => {
+        
+        
+            setCss(document.body, 'color', colors[i++]);
+            if(i>=colors.length){
+                i=0;
+            }
+            setCss(document.body, 'backgroundColor', colors[i]);
+            
+        }, 1000);
+     })
 }());
