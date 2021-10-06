4e28130d84876980ba1309c6876175477ad17282
Duvi Kappel <duvikappel@gmail.com>
2021-10-06 13:26:52 -0400

    HW 65 Incomplete

--- js/67Quiz/useCounters.js
+++ js/67Quiz/useCounters.js
@@ -3,7 +3,7 @@ window.app.getCount();
 window.app.increaseCounter();
 window.app.getCount();
 
-counter1=createCounters();
+counter1=window.app.createCounters();
 counter1.increaseCounter();
 counter1.increaseCounter();
 counter1.increaseCounter();
@@ -12,8 +12,15 @@ counter1.getCount();
 counter1.increaseCounter();
 counter1.getCount();
 
-counter2=createCounters();
+counter2=window.app.createCounters();
 counter2.increaseCounter();
 counter2.getCount();
 counter2.increaseCounter();
 counter2.getCount();
+
+counter3=window.app.createCounters();
+
+for ( let q=0; q<10; q++){
+    counter3.increaseCounter();
+}
+counter3.getCount();
