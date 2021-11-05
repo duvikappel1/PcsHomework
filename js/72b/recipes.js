74074739e8c09459eb1ef2901e2eb349bd2b20e2
Duvi Kappel <duvikappel@gmail.com>
2021-11-05 13:53:27 -0400

    Hw 72

--- js/72b/recipes.js
+++ js/72b/recipes.js
@@ -1,33 +1,57 @@
-/*global pcs*/
-
-console.log('js loaded');
-
 (function () {
   'use strict';
 
-  const recipeInput = $('#recipe');
-  const result= $('#result');
-  
+  const recipeSelect = $('#recipes');
+  const recipeNameElem = $('#recipe h2');
+  const name = $('#name');
+  const ingredients = $('#ingredients');
+  const recipeIngredientsList = $('#recipe ul');
+ 
 
-  $('#load').click(() => {
-    result.text('');
-   
-    fetch(recipeInput.val())
-      .then(r => {
-        if (!r.ok) {
-          throw new Error(`${r.status} ${r.statusText}`);
-        }
-        return r.text();
-      })
-      .then(t => {
-        console.log(t);
-        result.text(`${t.food}`);
-      })
-      .catch(err => {
-        pcs.messageBox.show(err, true);
-        throw new Error('OOPS!!!');
-      })
-      
+  async function loadChoices() {
+    try {
+      const response = await fetch('recipe.json');
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      const choices = await response.json();
+      if (choices) {
+        choices.forEach(recipe => {
+          recipeSelect.append(`<option value="${recipe.id}">${recipe.name}</option>`);
+        });
+    }
+  }
+      catch(e) {
+      console.error('OOPS, ERROR', e);
+    }
+  }
+    loadChoices();
+
+     recipeSelect.change(async function () {
+      console.log(this.value);
+      const request = await fetch(`${this.value}.json`)
+      if (!request) {
+        throw new Error(`${request.status} ${request.statusText}`);
+    }
+      const recipe = await request.json();
+      if(recipe){
+      console.log(recipe);
      
-  });
+    }
+      recipeNameElem.text(recipe.name);
+     // recipePictureElem.attr('src', recipe.picture);
+
+      recipeIngredientsList.empty();
+      ingredients.empty();
+      name.empty();
+      name.append(recipe.name)
+      ingredients.append("Ingredients");
+      recipe.ingredients.forEach(i => {
+      recipeIngredientsList.append(`<li>${i}</li>`);
+      })
+    });
+    
+   
+  
+ 
 }());
