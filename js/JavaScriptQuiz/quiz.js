395014fac9c5f8b12f18a77276d38a83653535f0
Duvi Kappel <duvikappel@gmail.com>
2021-11-30 19:21:31 -0500

    83Quiz

---
+++ js/JavaScriptQuiz/quiz.js
@@ -0,0 +1,50 @@
+//This is what I thought should work but it doesn't.
+(function(){
+  'use strict';
+  
+  const display = document.getElementById('orders');
+
+  fetch('quiz.json')
+    .then(r => r.json())
+    .then(t => {
+      t.forEach(order => { 
+        const item = new Item(order.items);
+        const order3 = new Order(order.customer, order.address, item);
+        order3.print();
+      });
+    });
+  
+
+  class Item {
+    
+    constructor(items) {
+     for(let x=0; x < items.length; x++){
+       this.item = item;
+       this.price = price/quantity;
+       this.quantity = quantity;
+       }   
+    }
+
+    print() {
+      display.append(`The price for ${this.name} is ${this.price}. There are ${this.quantity} in stock.`);
+    }
+}
+
+  class Order {
+    
+    constructor(customerName, customerAddress, item) {
+      this.customerName = customerName;
+      this.customerAddress = customerAddress;
+      this.item = item;
+
+    }
+
+    print() {
+       
+      display.append(`customer: ${this.customerName} Address: ${this.customerAddress}`);
+      display.append(`item: ${this.item.item.item} quantity: ${this.item.quantity} price: ${this.item.price}}`) 
+      
+    }
+}
+
+}())
