7913b954ce98701c349610d75d7cab82560490d9
slubowsky <270589+slubowsky@users.noreply.github.com>
2021-12-06 16:21:15 -0500

    quiz comments

--- js/JavaScriptQuiz/quiz.js
+++ js/JavaScriptQuiz/quiz.js
@@ -1,28 +1,32 @@
 //This is what I thought should work but it doesn't.
 (function(){
   'use strict';
-  
+
   const display = document.getElementById('orders');
 
   fetch('quiz.json')
     .then(r => r.json())
     .then(t => {
-      t.forEach(order => { 
+      t.forEach(order => {
         const item = new Item(order.items);
+        // SL - we would probably really want to be saving all the orders in a array somewhere, not just creating them and calling print
         const order3 = new Order(order.customer, order.address, item);
         order3.print();
       });
     });
-  
+
 
   class Item {
-    
+
     constructor(items) {
+      // SL - this class should represent a single item so wouldnt make sense to pass multiple items in to the ctor
+      // even if we for some reason wanted to model "items" (which doesnt really make sense) how would looping through
+      // and setting this.x be useful? this.x can only be one thing - and will be whatever last item data was
      for(let x=0; x < items.length; x++){
-       this.item = item;
-       this.price = price/quantity;
-       this.quantity = quantity;
-       }   
+       this.item = item; // SL - what is item? Do you mean items[x]?
+       this.price = price/quantity; // SL - what is price, quantity? Do you mean items[x].price, items[x].quantity?
+       this.quantity = quantity; // SL - what is quantity? Do you mean items[x].quantity?
+       }
     }
 
     print() {
@@ -31,7 +35,7 @@
 }
 
   class Order {
-    
+
     constructor(customerName, customerAddress, item) {
       this.customerName = customerName;
       this.customerAddress = customerAddress;
@@ -40,14 +44,14 @@
     }
 
     print() {
-       
+
       display.append(`customer: ${this.customerName} Address: ${this.customerAddress}`);
       for(let x = 0; x < this.item.length; x++){
-        display.append(`item: ${this.item.item} quantity: ${this.item.quantity} price: ${this.item.price}`) 
+        display.append(`item: ${this.item.item} quantity: ${this.item.quantity} price: ${this.item.price}`)
       }
-      
-      
     }
 }
 
 }())
+
+// SL - looks to me like you need more practice. Have you been doing all the homeworks? I dont see them in your repo...
